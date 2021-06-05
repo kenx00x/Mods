f47e96ea7069aad23df32cc90ddb761b2d3cd4f6
kenx00x <kenx00x@gmail.com>
2021-06-05 12:55:40 +0200

    added hidedelay timer

--- Mods/BepInEx/Valheim/Up To Date/Valheim Chat Tweaks/Class1.cs
+++ Mods/BepInEx/Valheim/Up To Date/Valheim Chat Tweaks/Class1.cs
@@ -4,17 +4,19 @@ using HarmonyLib;
 using Jotunn.Utils;
 namespace Valheim_Chat_Tweaks
 {
-    [BepInPlugin("kenx00x.ChatTweaks", "Chat Tweaks", "1.0.0")]
+    [BepInPlugin("kenx00x.ChatTweaks", "Chat Tweaks", "1.1.0")]
     [BepInProcess("valheim.exe")]
     [BepInDependency(Jotunn.Main.ModGuid)]
     [NetworkCompatibility(CompatibilityLevel.EveryoneMustHaveMod, VersionStrictness.Patch)]
     public class ChatTweaks : BaseUnityPlugin
     {
         static public ConfigEntry<bool> enableAlwaysShout;
+        static public ConfigEntry<float> hideDelay;
         private readonly Harmony harmony = new Harmony("kenx00x.ChatTweaks");
         public void Awake()
         {
             enableAlwaysShout = Config.Bind("General","AlwaysShout",false,"Enable or disable to always shout");
+            hideDelay = Config.Bind("General","ChatHideDelay",10f,"This value configures how fast the chat hides after sending/receiving a message.");
             harmony.PatchAll();
         }
         [HarmonyPatch(typeof(Chat), "SendText")]
@@ -32,9 +34,13 @@ namespace Valheim_Chat_Tweaks
         [HarmonyPatch(typeof(Chat), "OnNewChatMessage")]
         public class Chat_Patch
         {
-            public static void Prefix(ref float ___m_hideTimer)
+            public static void Prefix(ref float ___m_hideTimer, ref float ___m_hideDelay)
             {
                 ___m_hideTimer = 0;
+                if (hideDelay.Value >= 1f)
+                {
+                    ___m_hideDelay = hideDelay.Value;
+                }
             }
         }
     }
