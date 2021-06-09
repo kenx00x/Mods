6578a466853d5046e08a02be39c7518a37fc1073
kenx00x <kenx00x@gmail.com>
2021-06-09 21:31:56 +0200

    added vulkan support

--- Mods/BepInEx/Valheim/Up To Date/Valheim Minecraft Steve Hairstyle/Class1.cs
+++ Mods/BepInEx/Valheim/Up To Date/Valheim Minecraft Steve Hairstyle/Class1.cs
@@ -6,7 +6,7 @@ using Jotunn.Utils;
 using UnityEngine;
 namespace Valheim_Minecraft_Steve_Hairstyle
 {
-    [BepInPlugin("kenx00x.MinecraftSteveHairstyle", "Minecraft Steve Hairstyle", "1.1.2")]
+    [BepInPlugin("kenx00x.MinecraftSteveHairstyle", "Minecraft Steve Hairstyle", "1.2.0")]
     [BepInProcess("valheim.exe")]
     [BepInDependency(Jotunn.Main.ModGuid)]
     [NetworkCompatibility(CompatibilityLevel.EveryoneMustHaveMod, VersionStrictness.Patch)]
@@ -15,7 +15,7 @@ namespace Valheim_Minecraft_Steve_Hairstyle
         public void Awake()
         {
             AssetBundle embeddedResourceBundle = AssetUtils.LoadAssetBundleFromResources("hairminecraftsteve", typeof(MinecraftSteveHairstyle).Assembly);
-            GameObject steveHairstyle = embeddedResourceBundle.LoadAsset<GameObject>("Assets/customitems/hairminecraftsteve.prefab");
+            GameObject steveHairstyle = embeddedResourceBundle.LoadAsset<GameObject>("Assets/customitems/steve/hairminecraftsteve.prefab");
             Jotunn.Logger.LogInfo($"Embedded resources: {string.Join(",", typeof(MinecraftSteveHairstyle).Assembly.GetManifestResourceNames())}");
 
             CustomItem customItem = new CustomItem(steveHairstyle, false);
