f9cc1f063d0ea3acdc3633737066873f14c70b66
kenx00x <kenx00x@gmail.com>
2021-07-21 01:29:33 +0200

    made a rimworld mod

---
+++ Mods/Rimworld/MoreMaxMemes/Source/1.3/MoreMaxMemes/Class1.cs
@@ -0,0 +1,25 @@
+﻿using HarmonyLib;
+using Verse;
+using RimWorld;
+using System.Reflection;
+
+namespace MoreMaxMemes
+{
+    [StaticConstructorOnStartup]
+    public class MoreMaxMemes : Mod
+    {
+        public MoreMaxMemes(ModContentPack content) : base(content)
+        {
+            var harmony = new Harmony("kenx00x.MoreMaxMemes");
+            harmony.PatchAll();
+        }
+    }
+    [HarmonyPatch(typeof(IdeoFoundation), "InitPrecepts")]
+    public class Chat_Patch
+    {
+        public static void Prefix(ref IntRange ___MemeCountRangeAbsolute)
+        {
+            ___MemeCountRangeAbsolute = new IntRange(0, 100);
+        }
+    }
+}
