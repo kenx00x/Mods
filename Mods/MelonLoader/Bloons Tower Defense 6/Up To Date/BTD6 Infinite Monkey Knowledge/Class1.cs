c2ba34ef51e82f2651753c654314ac61dc0ae947
kenx00x <kenx00x@gmail.com>
2021-09-12 21:39:30 +0200

    using BloonsTD6 Mod Helper and updated for melonloader 0.4.3

--- Mods/MelonLoader/Bloons Tower Defense 6/Up To Date/BTD6 Infinite Monkey Knowledge/Class1.cs
+++ Mods/MelonLoader/Bloons Tower Defense 6/Up To Date/BTD6 Infinite Monkey Knowledge/Class1.cs
@@ -1,43 +1,19 @@
 ﻿using Assets.Scripts.Unity;
 using Assets.Scripts.Unity.UI_New.Main;
-using Harmony;
+using HarmonyLib;
 using MelonLoader;
-using System.IO;
-[assembly: MelonInfo(typeof(BTD6_Infinite_Monkey_Knowledge.Class1), "Infinite Monkey Knowledge", "1.2.0", "kenx00x")]
+using BTD_Mod_Helper;
+using BTD_Mod_Helper.Api.ModOptions;
+[assembly: MelonInfo(typeof(BTD6_Infinite_Monkey_Knowledge.Class1), "Infinite Monkey Knowledge", "2.0.0", "kenx00x")]
 [assembly: MelonGame("Ninja Kiwi", "BloonsTD6")]
 namespace BTD6_Infinite_Monkey_Knowledge
 {
-    public class Class1 : MelonMod
+    public class Class1 : BloonsTD6Mod
     {
-        public static string dir = $"{Directory.GetCurrentDirectory()}\\Mods\\InfiniteMonkeyKnowledge";
-        public static string config = $"{dir}\\config.txt";
-        public static int amount = 999999999;
+        public static ModSettingInt amount = new ModSettingInt(999999999);
         public override void OnApplicationStart()
         {
             MelonLogger.Msg("Infinite Monkey Knowledge loaded!");
-            Directory.CreateDirectory($"{dir}");
-            if (File.Exists(config))
-            {
-                MelonLogger.Msg("Reading config file");
-                using (StreamReader sr = File.OpenText(config))
-                {
-                    string s = "";
-                    while ((s = sr.ReadLine()) != null)
-                    {
-                        amount = int.Parse(s.Substring(s.IndexOf(char.Parse("=")) + 1));
-                    }
-                }
-                MelonLogger.Msg("Done reading");
-            }
-            else
-            {
-                MelonLogger.Msg("Creating config file");
-                using (StreamWriter sw = File.CreateText(config))
-                {
-                    sw.WriteLine("Amount=999999999");
-                }
-                MelonLogger.Msg("Done Creating");
-            }
         }
         [HarmonyPatch(typeof(MainMenu), "Open")]
         public class TitleScreen_Patch
