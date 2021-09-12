373e0c532b807d3f3dd254e977d7f164a1222fe3
kenx00x <kenx00x@gmail.com>
2021-09-13 01:05:42 +0200

    updated for melonloader 0.4.3

--- Mods/MelonLoader/Bloons Tower Defense 6/Up To Date/BTD6 Golden Bloon In Sandbox/Class1.cs
+++ Mods/MelonLoader/Bloons Tower Defense 6/Up To Date/BTD6 Golden Bloon In Sandbox/Class1.cs
@@ -2,9 +2,9 @@
 using Assets.Scripts.Unity;
 using Il2CppSystem.Collections.Generic;
 using Assets.Scripts.Models.Bloons;
-using Harmony;
+using HarmonyLib;
 using Assets.Scripts.Unity.UI_New.InGame.BloonMenu;
-[assembly: MelonInfo(typeof(BTD6_Golden_Bloon_In_Sandbox.Class1), "Golden Bloon In Sandbox", "1.1.0", "kenx00x")]
+[assembly: MelonInfo(typeof(BTD6_Golden_Bloon_In_Sandbox.Class1), "Golden Bloon In Sandbox", "2.0.0", "kenx00x")]
 [assembly: MelonGame("Ninja Kiwi", "BloonsTD6")]
 namespace BTD6_Golden_Bloon_In_Sandbox
 {
