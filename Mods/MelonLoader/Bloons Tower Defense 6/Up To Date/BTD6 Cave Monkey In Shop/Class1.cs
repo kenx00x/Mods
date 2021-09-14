0fd6c17c1d9d6dacac8566557909667dcd4d20ae
kenx00x <kenx00x@gmail.com>
2021-09-14 12:28:06 +0200

    using BloonsTD6 Mod Helper and updated for melonloader 0.4.3 and simplified code

--- Mods/MelonLoader/Bloons Tower Defense 6/Up To Date/BTD6 Cave Monkey In Shop/Class1.cs
+++ Mods/MelonLoader/Bloons Tower Defense 6/Up To Date/BTD6 Cave Monkey In Shop/Class1.cs
@@ -5,45 +5,21 @@ using Assets.Scripts.Models.TowerSets;
 using Assets.Scripts.Simulation.Input;
 using Assets.Scripts.Unity;
 using Assets.Scripts.Unity.UI_New.Upgrade;
-using Harmony;
+using BTD_Mod_Helper;
+using BTD_Mod_Helper.Api.ModOptions;
+using HarmonyLib;
 using Il2CppSystem.Collections.Generic;
 using MelonLoader;
-using System.IO;
-[assembly: MelonInfo(typeof(BTD6_Cave_Monkey_In_Shop.Class1), "Cave Monkey In Shop", "1.2.0", "kenx00x")]
+[assembly: MelonInfo(typeof(BTD6_Cave_Monkey_In_Shop.Class1), "Cave Monkey In Shop", "2.0.0", "kenx00x")]
 [assembly: MelonGame("Ninja Kiwi", "BloonsTD6")]
 namespace BTD6_Cave_Monkey_In_Shop
 {
-    public class Class1 : MelonMod
+    public class Class1 : BloonsTD6Mod
     {
-        public static string dir = $"{Directory.GetCurrentDirectory()}\\Mods\\CaveMonkeyInShop";
-        public static string config = $"{dir}\\config.txt";
-        public static int caveMonkeyCost = 250;
+        public static ModSettingInt price = new ModSettingInt(250);
         public override void OnApplicationStart()
         {
             MelonLogger.Msg("Cave Monkey In Shop mod loaded");
-            Directory.CreateDirectory($"{dir}");
-            if (File.Exists(config))
-            {
-                MelonLogger.Msg("Reading config file");
-                using (StreamReader sr = File.OpenText(config))
-                {
-                    string s = "";
-                    while ((s = sr.ReadLine()) != null)
-                    {
-                        caveMonkeyCost = int.Parse(s.Substring(s.IndexOf(char.Parse("=")) + 1));
-                    }
-                }
-                MelonLogger.Msg("Done reading");
-            }
-            else
-            {
-                MelonLogger.Msg("Creating config file");
-                using (StreamWriter sw = File.CreateText(config))
-                {
-                    sw.WriteLine("CaveMonkeyCost=250");
-                }
-                MelonLogger.Msg("Done Creating");
-            }
         }
         [HarmonyPatch(typeof(ProfileModel), "Validate")]
         public class ProfileModel_Patch
@@ -74,7 +50,7 @@ namespace BTD6_Cave_Monkey_In_Shop
                 {
                     powerModel.tower.icon = powerModel.icon;
                 }
-                powerModel.tower.cost = caveMonkeyCost;
+                powerModel.tower.cost = price;
                 powerModel.tower.towerSet = "Support";
             }
         }
@@ -82,24 +58,22 @@ namespace BTD6_Cave_Monkey_In_Shop
         public class TowerInventory_Patch
         {
             [HarmonyPrefix]
-            public static bool Prefix(ref List<TowerDetailsModel> allTowersInTheGame)
+            public static void Prefix(ref List<TowerDetailsModel> allTowersInTheGame)
             {
                 ShopTowerDetailsModel powerDetails = new ShopTowerDetailsModel("CaveMonkey", 1, 0, 0, 0, -1, 0, null);
                 allTowersInTheGame.Add(powerDetails);
-                return true;
             }
         }
         [HarmonyPatch(typeof(UpgradeScreen), "UpdateUi")]
         public class UpgradeScreen_Patch
         {
             [HarmonyPrefix]
-            public static bool Prefix(ref string towerId)
+            public static void Prefix(ref string towerId)
             {
                 if (towerId.Contains("CaveMonkey"))
                 {
                     towerId = "DartMonkey";
                 }
-                return true;
             }
         }
     }
