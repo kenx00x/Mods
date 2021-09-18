e5a5ef966b299978eb25cd3516a1b16566ba9327
kenx00x <kenx00x@gmail.com>
2021-09-18 22:29:44 +0200

    New mod max player level

---
+++ Mods/MelonLoader/Bloons Tower Defense 6/Up To Date/BTD6 Max Player Level/Class1.cs
@@ -0,0 +1,31 @@
+﻿using Assets.Scripts.Unity;
+using Assets.Scripts.Unity.UI_New.Main;
+using BTD_Mod_Helper;
+using BTD_Mod_Helper.Api.ModOptions;
+using HarmonyLib;
+using MelonLoader;
+[assembly: MelonInfo(typeof(BTD6_Max_Player_level.Class1), "Max Player Level", "1.0.0", "kenx00x")]
+[assembly: MelonGame("Ninja Kiwi", "BloonsTD6")]
+namespace BTD6_Max_Player_level
+{
+    public class Class1 : BloonsTD6Mod
+    {
+        public static ModSettingDouble xp = new ModSettingDouble(999999999);
+        public static ModSettingDouble VeteranXp = new ModSettingDouble(999999999);
+        public override void OnApplicationStart()
+        {
+            MelonLogger.Msg("Max Player Level loaded!");
+        }
+        [HarmonyPatch(typeof(MainMenu), "Open")]
+        public class TitleScreen_Patch
+        {
+            [HarmonyPostfix]
+            public static void Postfix()
+            {
+                Game.instance.playerService.Player.Data.xp.Value = xp;
+                Game.instance.playerService.Player.Data.veteranXp.Value = VeteranXp;
+                Game.instance.playerService.Player.CheckAndCorrectLevelBasedOnPlayerXp();
+            }
+        }
+    }
+}
---
+++ Mods/MelonLoader/Bloons Tower Defense 6/Up To Date/BTD6 Max Player Level/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("BTD6 Max Player Level")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BTD6 Max Player Level")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("04e9af87-d086-4918-9291-0bc3a8b1307d")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
