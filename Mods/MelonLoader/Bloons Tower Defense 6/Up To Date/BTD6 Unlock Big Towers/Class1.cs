8566616a0481609eaf768d78d2ca168d145259af
kenx00x <kenx00x@gmail.com>
2021-10-16 14:01:20 +0200

    big towers

---
+++ Mods/MelonLoader/Bloons Tower Defense 6/Up To Date/BTD6 Unlock Big Towers/Class1.cs
@@ -0,0 +1,25 @@
+﻿using MelonLoader;
+using Assets.Scripts.Unity;
+using HarmonyLib;
+using Assets.Scripts.Unity.UI_New.Main;
+[assembly: MelonInfo(typeof(BTD6_Unlock_Big_Towers.Class1), "Unlock Big Towers", "1.0.0", "kenx00x")]
+[assembly: MelonGame("Ninja Kiwi", "BloonsTD6")]
+namespace BTD6_Unlock_Big_Towers
+{
+    public class Class1 : MelonMod
+    {
+        public override void OnApplicationStart()
+        {
+            MelonLogger.Msg("Unlock Big Towers loaded");
+        }
+        [HarmonyPatch(typeof(MainMenu), "Open")]
+        public class TitleScreen_Patch
+        {
+            [HarmonyPostfix]
+            public static void Postfix()
+            {
+                Game.instance.playerService.Player.Data.unlockedBigTowers = true;
+            }
+        }
+    }
+}
