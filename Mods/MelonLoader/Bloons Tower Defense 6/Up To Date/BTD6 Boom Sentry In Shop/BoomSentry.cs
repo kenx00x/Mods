592b2b85ec181045b73f4ebf9f32ae40ff63a122
kenx00x <kenx00x@gmail.com>
2022-01-23 19:57:59 +0100

    update

---
+++ Mods/MelonLoader/Bloons Tower Defense 6/Up To Date/BTD6 Boom Sentry In Shop/BoomSentry.cs
@@ -0,0 +1,21 @@
+﻿using Assets.Scripts.Models.Towers;
+using Assets.Scripts.Unity;
+using Assets.Scripts.Utils;
+using BTD_Mod_Helper.Api.Towers;
+namespace BTD6_Boom_Sentry_In_Shop
+{
+    public class BoomSentry : ModTower
+    {
+        public override SpriteReference PortraitReference => Game.instance.model.GetTowerWithName("SentryBoom").portrait;
+        public override SpriteReference IconReference => Game.instance.model.GetTowerWithName("SentryBoom").icon;
+        public override string TowerSet => SUPPORT;
+        public override string BaseTower => TowerType.SentryBoom;
+        public override int Cost => Main.Price;
+        public override int TopPathUpgrades => 0;
+        public override int MiddlePathUpgrades => 0;
+        public override int BottomPathUpgrades => 0;
+        public override string Description => "Boom Sentry";
+        public override void ModifyBaseTowerModel(TowerModel towerModel)
+        { }
+    }
+}
--- Mods/MelonLoader/Bloons Tower Defense 6/Up To Date/BTD6 Boom Sentry In Shop/Main.cs
+++ Mods/MelonLoader/Bloons Tower Defense 6/Up To Date/BTD6 Boom Sentry In Shop/Main.cs
@@ -1,93 +1,16 @@
-﻿using Assets.Main.Scenes;
-using Assets.Scripts.Models;
-using Assets.Scripts.Models.Profile;
-using Assets.Scripts.Models.Towers;
-using Assets.Scripts.Models.TowerSets;
-using Assets.Scripts.Simulation.Input;
-using Assets.Scripts.Unity;
-using Assets.Scripts.Unity.UI_New.Upgrade;
-using BTD_Mod_Helper;
+﻿using BTD_Mod_Helper;
 using BTD_Mod_Helper.Api.ModOptions;
-using HarmonyLib;
-using Il2CppSystem.Collections.Generic;
 using MelonLoader;
-[assembly: MelonInfo(typeof(BTD6_Boom_Sentry_In_Shop.Class1), "Boom Sentry In Shop", "2.0.0", "kenx00x")]
+[assembly: MelonInfo(typeof(BTD6_Boom_Sentry_In_Shop.Main), "Boom Sentry In Shop", "3.0.0", "kenx00x")]
 [assembly: MelonGame("Ninja Kiwi", "BloonsTD6")]
 namespace BTD6_Boom_Sentry_In_Shop
 {
-    public class Class1 : BloonsTD6Mod
+    public class Main : BloonsTD6Mod
     {
-        public static ModSettingInt price = new ModSettingInt(500);
+        public static readonly ModSettingInt Price = new ModSettingInt(500);
         public override void OnApplicationStart()
         {
-            MelonLogger.Msg("Boom Sentry In Shop mod loaded");
-        }
-        [HarmonyPatch(typeof(ProfileModel), "Validate")]
-        public class ProfileModel_Patch
-        {
-            [HarmonyPostfix]
-            public static void Postfix(ProfileModel __instance)
-            {
-                HashSet<string> unlockedTowers = __instance.unlockedTowers;
-                if (unlockedTowers.Contains("SentryBoom"))
-                {
-                    MelonLogger.Msg("Boom Sentry already unlocked");
-                }
-                else
-                {
-                    MelonLogger.Msg("unlocking Boom Sentry");
-                    unlockedTowers.Add("SentryBoom");
-                }
-            }
-        }
-        [HarmonyPatch(typeof(TitleScreen), "UpdateVersion")]
-        public class TitleScreen_Patch
-        {
-            [HarmonyPostfix]
-            public static void Postfix()
-            {
-                TowerModel towerModel = Game.instance.model.GetTowerWithName("SentryBoom");
-                if (towerModel.icon == null)
-                {
-                    towerModel.icon = towerModel.portrait;
-                }
-                towerModel.cost = price;
-                towerModel.towerSet = "Support";
-                towerModel.radius = 6;
-                towerModel.radiusSquared = 36;
-                towerModel.footprint.doesntBlockTowerPlacement = false;
-                List<Model> temp = new List<Model>();
-                for (int i = 0; i < towerModel.behaviors.Length; i++)
-                {
-                    if (towerModel.behaviors[i].name != "TowerExpireModel_")
-                    {
-                        temp.Add(towerModel.behaviors[i]);
-                    }
-                }
-                towerModel.behaviors = (UnhollowerBaseLib.Il2CppReferenceArray<Model>)temp.ToArray();
-            }
-        }
-        [HarmonyPatch(typeof(TowerInventory), "Init")]
-        public class TowerInventory_Patch
-        {
-            [HarmonyPrefix]
-            public static void Prefix(ref List<TowerDetailsModel> allTowersInTheGame)
-            {
-                ShopTowerDetailsModel towerDetails = new ShopTowerDetailsModel("SentryBoom", 1, 0, 0, 0, -1, 0, null);
-                allTowersInTheGame.Add(towerDetails);
-            }
-        }
-        [HarmonyPatch(typeof(UpgradeScreen), "UpdateUi")]
-        public class UpgradeScreen_Patch
-        {
-            [HarmonyPrefix]
-            public static void Prefix(ref string towerId)
-            {
-                if (towerId.Contains("SentryBoom"))
-                {
-                    towerId = "DartMonkey";
-                }
-            }
+            LoggerInstance.Msg("Boom Sentry In Shop mod loaded");
         }
     }
 }
