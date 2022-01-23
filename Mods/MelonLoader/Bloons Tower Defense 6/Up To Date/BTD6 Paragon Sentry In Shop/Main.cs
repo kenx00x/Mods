22f4b1d42588c8de99a6b87bebb894c10d5712c2
kenx00x <kenx00x@gmail.com>
2022-01-23 20:50:09 +0100

    update

---
+++ Mods/MelonLoader/Bloons Tower Defense 6/Up To Date/BTD6 Paragon Sentry In Shop/Main.cs
@@ -0,0 +1,19 @@
+﻿using BTD_Mod_Helper;
+using BTD_Mod_Helper.Api.ModOptions;
+using MelonLoader;
+using Main = BTD6_Paragon_Sentry_In_Shop.Main;
+
+[assembly: MelonInfo(typeof(Main), "Paragon Sentry In Shop", "3.0.0", "kenx00x")]
+[assembly: MelonGame("Ninja Kiwi", "BloonsTD6")]
+namespace BTD6_Paragon_Sentry_In_Shop
+{
+    public class Main : BloonsTD6Mod
+    {
+        public static readonly ModSettingInt Price = new ModSettingInt(5000);
+
+        public override void OnApplicationStart()
+        {
+            LoggerInstance.Msg("Paragon Sentry In Shop mod loaded");
+        }
+    }
+}
