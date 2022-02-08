ac5fe4da46d146f8d278eba55ba0d02ccf8bb33b
kenx00x <kenx00x@gmail.com>
2022-02-08 05:48:16 +0100

    fixed

--- Mods/MelonLoader/Bloons Tower Defense 6/Up To Date/BTD6 All Trophy Store Items Unlocker/Main.cs
+++ Mods/MelonLoader/Bloons Tower Defense 6/Up To Date/BTD6 All Trophy Store Items Unlocker/Main.cs
@@ -1,38 +1,49 @@
-﻿using Assets.Scripts.Data;
+﻿using Assets.Main.Scenes;
+using Assets.Scripts.Data;
 using Assets.Scripts.Unity;
 using Assets.Scripts.Unity.UI_New.Main;
 using HarmonyLib;
 using Il2CppSystem.Collections.Generic;
 using MelonLoader;
-[assembly: MelonInfo(typeof(BTD6_All_Trophy_Store_Items_Unlocker.Class1), "All Trophy Store Items Unlocker", "2.0.0", "kenx00x")]
+using Main = BTD6_All_Trophy_Store_Items_Unlocker.Main;
+
+[assembly: MelonInfo(typeof(Main), "All Trophy Store Items Unlocker", "3.0.0", "kenx00x")]
 [assembly: MelonGame("Ninja Kiwi", "BloonsTD6")]
+
 namespace BTD6_All_Trophy_Store_Items_Unlocker
 {
-    public class Class1 : MelonMod
+    public class Main : MelonMod
     {
         public override void OnApplicationStart()
         {
             MelonLogger.Msg("All Trophy Store Items Unlocker loaded!");
         }
+
         [HarmonyPatch(typeof(MainMenu), "Open")]
-        public class TitleScreen_Patch
+        public class TitleScreenPatch
         {
             [HarmonyPostfix]
             public static void Postfix()
             {
-                List<string> testList = new List<string>();
+                var testList = new List<string>();
                 foreach (var item in Game.instance.playerService.Player.Data.trophyStorePurchasedItems)
-                {
                     testList.Add(item.Key);
-                }
                 foreach (var item in GameData._instance.trophyStoreItems.storeItems)
                 {
-                    if (!testList.Contains(item.id))
-                    {
-                        Game.instance.playerService.Player.AddTrophyStoreItem(item.id);
-                        MelonLogger.Msg($"Unlocked {item.id}");
-                    }
+                    if (testList.Contains(item.id)) continue;
+                    Game.instance.playerService.Player.AddTrophyStoreItem(item.id);
+                    MelonLogger.Msg($"Unlocked {item.id}");
                 }
+            }
+        }
+
+        [HarmonyPatch(typeof(InitialLoadingScreen), "Update")]
+        public class InitialLoadingScreenPatch
+        {
+            [HarmonyPostfix]
+            public static void Postfix()
+            {
+                Modding.isModdedClient = false;
             }
         }
     }
