17695a29af3ce0976d8c9ffa0334f21e6d5368a4
kenx00x <kenx00x@gmail.com>
2022-02-11 11:20:18 +0100

    fixed

--- Mods/MelonLoader/Bloons Tower Defense 6/Up To Date/BTD6 Infinite Monkey Money/Main.cs
+++ Mods/MelonLoader/Bloons Tower Defense 6/Up To Date/BTD6 Infinite Monkey Money/Main.cs
@@ -4,11 +4,12 @@ using HarmonyLib;
 using MelonLoader;
 using BTD_Mod_Helper;
 using BTD_Mod_Helper.Api.ModOptions;
-[assembly: MelonInfo(typeof(BTD6_Infinite_Monkey_Money.Class1), "Infinite Monkey Money", "2.0.0", "kenx00x")]
+using Assets.Main.Scenes;
+[assembly: MelonInfo(typeof(BTD6_Infinite_Monkey_Money.Main), "Infinite Monkey Money", "3.0.0", "kenx00x")]
 [assembly: MelonGame("Ninja Kiwi", "BloonsTD6")]
 namespace BTD6_Infinite_Monkey_Money
 {
-    public class Class1 : BloonsTD6Mod
+    public class Main : BloonsTD6Mod
     {
         public static ModSettingInt amount = new ModSettingInt(999999999);
         public override void OnApplicationStart()
@@ -22,6 +23,15 @@ namespace BTD6_Infinite_Monkey_Money
             public static void Postfix()
             {
                 Game.instance.playerService.Player.Data.monkeyMoney.Value = amount;
+            }
+        }
+        [HarmonyPatch(typeof(InitialLoadingScreen), "Update")]
+        public class InitialLoadingScreenPatch
+        {
+            [HarmonyPostfix]
+            public static void Postfix()
+            {
+                Modding.isModdedClient = false;
             }
         }
     }
