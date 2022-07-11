90e7030be41bc1dec8f0ac8971216c66ad2114b6
kenx00x <kenx00x@gmail.com>
2022-07-11 17:32:17 +0200

    fixed trophy mod

--- Mods/MelonLoader/Bloons Tower Defense 6/Up To Date/BTD6 All Trophy Store Items Unlocker/Main.cs
+++ Mods/MelonLoader/Bloons Tower Defense 6/Up To Date/BTD6 All Trophy Store Items Unlocker/Main.cs
@@ -7,7 +7,7 @@ using Il2CppSystem.Collections.Generic;
 using MelonLoader;
 using Main = BTD6_All_Trophy_Store_Items_Unlocker.Main;
 
-[assembly: MelonInfo(typeof(Main), "All Trophy Store Items Unlocker", "3.0.0", "kenx00x")]
+[assembly: MelonInfo(typeof(Main), "All Trophy Store Items Unlocker", "4.0.0", "kenx00x")]
 [assembly: MelonGame("Ninja Kiwi", "BloonsTD6")]
 
 namespace BTD6_All_Trophy_Store_Items_Unlocker
@@ -44,6 +44,26 @@ namespace BTD6_All_Trophy_Store_Items_Unlocker
             public static void Postfix()
             {
                 Modding.isModdedClient = false;
+            }
+        }
+        [HarmonyPatch(typeof(Modding), "CheckForMods")]
+        public class ModdingPatch
+        {
+            [HarmonyPostfix]
+            public static bool Prefix(ref bool __result)
+            {
+                __result = false;
+                return false;
+            }
+        }
+        [HarmonyPatch(typeof(Modding), "IsModdingLibrary")]
+        public class ModdingPatch2
+        {
+            [HarmonyPostfix]
+            public static bool Prefix(ref bool __result)
+            {
+                __result = false;
+                return false;
             }
         }
     }
