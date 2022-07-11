7ca1c40a94170b43a7c0d83b30431437f6862af1
kenx00x <kenx00x@gmail.com>
2022-07-11 18:18:00 +0200

    cookie clicker

---
+++ Mods/Cookie Clicker/dalgonaUmbrella/main.js
@@ -0,0 +1,6 @@
+Game.registerMod("dalgona umbrella",{
+	init:function(){
+		Game.Loader.Replace('perfectCookie.png',this.dir+'/dalgona umbrella.png');
+		Game.Loader.Replace('brokenCookie.png',this.dir+'/broken dalgona.png');
+	}
+});
