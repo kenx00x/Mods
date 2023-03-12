6d1e8ce510d2ecf9bc258a5a5a0880ece5096292
Steven <107305979+Stiefanek@users.noreply.github.com>
2023-03-12 23:56:36 +0100

    Update CaveMonkey.cs

--- Mods/Bloons Tower Defense 6/Maintained/BTD6 All The Towers In Shop/CaveMonkey.cs
+++ Mods/Bloons Tower Defense 6/Maintained/BTD6 All The Towers In Shop/CaveMonkey.cs
@@ -12,7 +12,7 @@ namespace BTD6_All_The_Towers_In_Shop
         public override SpriteReference IconReference => Game.instance.model.GetPowerWithName("CaveMonkey").icon;
 		public override TowerSet TowerSet => TowerSet.Primary;
 		public override string BaseTower => TowerType.CaveMonkey;
-        public override int Cost => Main.CaveMonkeyPrice;
+        public override int Cost => 750;
         public override int TopPathUpgrades => 0;
         public override int MiddlePathUpgrades => 0;
         public override int BottomPathUpgrades => 0;
