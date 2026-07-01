14cecc6687233b1d0530d89f528f1d7429a97cc7
kenx00x <kenx00x@gmail.com>
2026-07-01 21:30:55 +0200

    add vr libs

---
+++ SuchArt VR/Helpers/Logs.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SuchArt_VR.Helpers
+{
+    internal class Logs
+    {
+    }
+}
--- SuchArt VR/VRPlugin.cs
+++ SuchArt VR/VRPlugin.cs
@@ -1,12 +1,15 @@
-﻿using BepInEx;
+﻿using AS;
+using BepInEx;
 using HarmonyLib;
 using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.Runtime;
+using System.IO;
+using System.Reflection;
+using Unity.XR.OpenVR;
 using UnityEngine;
-using UnityEngine.XR;
+using UnityEngine.XR.Management;
+using UnityEngine.XR.OpenXR;
 
 namespace SuchArtVRMod
 {
@@ -14,28 +17,94 @@ namespace SuchArtVRMod
     public class VRPlugin : BaseUnityPlugin
     {
         // Metadata uniquely identifying your mod
-        public const string PluginGUID = "com.kenx00x.suchart.vrmod";
-        public const string PluginName = "SuchArt VR Mod";
-        public const string PluginVersion = "1.0.0";
+        public const string PLUGIN_GUID = "com.kenx00x.suchart.vrmod";
+        public const string PLUGIN_NAME = "SuchArt VR Mod";
+        public const string PLUGIN_VERSION = "1.0.0";
 
-        public static VRPlugin Instance { get; private set; }
-        private readonly Harmony _harmony = new Harmony(PluginGUID);
+        public static string gameExePath = Process.GetCurrentProcess().MainModule.FileName;
+        public static string gamePath = Path.GetDirectoryName(gameExePath);
+        public static string HMDModel = "";
+
+        public static UnityEngine.XR.Management.XRManagerSettings managerSettings = null;
+
+        public static List<UnityEngine.XR.XRDisplaySubsystemDescriptor> displaysDescs = new List<UnityEngine.XR.XRDisplaySubsystemDescriptor>();
+        public static List<UnityEngine.XR.XRDisplaySubsystem> displays = new List<UnityEngine.XR.XRDisplaySubsystem>();
+        public static UnityEngine.XR.XRDisplaySubsystem MyDisplay = null;
+
+        public static GameObject SecondEye = null;
+        public static Camera SecondCam = null;
+
+        public class MyStaticMB : MonoBehaviour
+        {
+        }
+
+        //Variable reference for the class
+        public static MyStaticMB myStaticMB;
 
         private void Awake()
         {
-            Instance = this;
-            Logger.LogInfo("[SuchArtVR] Mod by kenx00x initializing...");
+            // Plugin startup logic
+            Logger.LogInfo($"Plugin {PLUGIN_GUID} is loaded!");
 
-            try
+           // new AssetLoader();
+
+            Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly());
+
+            //If the instance not exit the first time we call the static class
+            if (myStaticMB == null)
             {
-                // Inject all Harmony patches compiled into this assembly
-                _harmony.PatchAll();
-                Logger.LogInfo("[SuchArtVR] Harmony patches applied successfully.");
+                //Create an empty object called MyStatic
+                GameObject gameObject = new GameObject("MyStatic");
+
+
+                //Add this script to the object
+                myStaticMB = gameObject.AddComponent<MyStaticMB>();
             }
-            catch (Exception ex)
-            {
-                Logger.LogError($"[SuchArtVR] Critical failure applying patches: {ex.Message}");
-            }
+
+            myStaticMB.StartCoroutine(InitVRLoader());
+        }
+
+        public static System.Collections.IEnumerator InitVRLoader()
+        {
+
+            SteamVR_Actions.PreInitialize();
+
+            var generalSettings = ScriptableObject.CreateInstance<XRGeneralSettings>();
+            managerSettings = ScriptableObject.CreateInstance<XRManagerSettings>();
+            var xrLoader = ScriptableObject.CreateInstance<OpenVRLoader>();
+
+
+            var settings = OpenVRSettings.GetSettings();
+            settings.StereoRenderingMode = OpenVRSettings.StereoRenderingModes.MultiPass;
+
+
+            generalSettings.Manager = managerSettings;
+
+            managerSettings.loaders.Clear();
+            managerSettings.loaders.Add(xrLoader);
+
+            managerSettings.InitializeLoaderSync(); ;
+
+
+            XRGeneralSettings.AttemptInitializeXRSDKOnLoad();
+            XRGeneralSettings.AttemptStartXRSDKOnBeforeSplashScreen();
+
+            SteamVR.Initialize(true);
+
+
+            SubsystemManager.GetInstances(displays);
+            MyDisplay = displays[0];
+            MyDisplay.Start();
+
+            Logs.WriteInfo("SteamVR hmd modelnumber: " + SteamVR.instance.hmd_ModelNumber);
+            HMDModel = SteamVR.instance.hmd_ModelNumber;
+
+            new VRInputManager();
+
+            Logs.WriteInfo("Reached end of InitVRLoader");
+
+            yield return null;
+
         }
     }
 }
