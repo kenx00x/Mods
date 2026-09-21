16b6fda580adbc095bf9d48c823dfad0a21472cf
kenx00x <kenx00x@gmail.com>
2026-09-21 22:52:44 +0200

    fix

--- Mods/Bloons Tower Defense 6/Maintained/BTD6 Free Towers/Class1.cs
+++ Mods/Bloons Tower Defense 6/Maintained/BTD6 Free Towers/Class1.cs
@@ -1,15 +1,18 @@
 ﻿using BTD_Mod_Helper;
 using BTD_Mod_Helper.Api.ModOptions;
+using BTD_Mod_Helper.Extensions;
 using HarmonyLib;
 using Il2CppAssets.Scripts.Models.Towers;
 using Il2CppAssets.Scripts.Models.Towers.Upgrades;
 using Il2CppAssets.Scripts.Unity;
-using Il2CppAssets.Scripts.Unity.Bridge;
 using Il2CppAssets.Scripts.Unity.Map;
-using Il2CppAssets.Scripts.Unity.UI_New.InGame.TowerSelectionMenu;
 using MelonLoader;
-[assembly: MelonInfo(typeof(freeTowers.Class1), "free towers and upgrades", "2.1.0", "kenx00x")]
-[assembly: MelonGame("Ninja Kiwi", "BloonsTD6")]
+using SimHero = Il2CppAssets.Scripts.Simulation.Towers.Behaviors.Hero;
+using SimTowerManager = Il2CppAssets.Scripts.Simulation.Towers.TowerManager;
+
+[assembly: MelonInfo(typeof(freeTowers.Class1), "free towers and upgrades", "2.1.1", "kenx00x")]
+[assembly: MelonGame("Ninja Kiwi")]
+
 namespace freeTowers
 {
     public class Class1 : BloonsTD6Mod
@@ -17,19 +20,14 @@ namespace freeTowers
         public static ModSettingInt towerCost = new ModSettingInt(0);
         public static ModSettingInt upgradeCost = new ModSettingInt(0);
         public static ModSettingDouble heroUpgradeCost = new ModSettingDouble(0);
+
+        static bool buyingHeroLevel;
+
         public override void OnApplicationStart()
         {
             MelonLogger.Msg("Free towers and upgrades mod loaded");
         }
-        [HarmonyPatch(typeof(TowerSelectionMenu), "UpdateHeroBooster")]
-        public class FreeHeroUpgrade_Patch
-        {
-            [HarmonyPostfix]
-            public static void Postfix(TowerToSimulation tower)
-            {
-                tower.hero.hero.heroModel.costPerXpToLevel = (float)heroUpgradeCost/1000;
-            }
-        }
+
         [HarmonyPatch(typeof(MapLoader), "Load")]
         public class MapLoader_Patch
         {
@@ -39,11 +37,54 @@ namespace freeTowers
                 foreach (TowerModel tower in Game.instance.model.towers)
                 {
                     tower.cost = towerCost;
+
+                    if (tower.IsHero())
+                    {
+                        var heroModel = tower.GetHeroModel();
+                        if (heroModel != null) heroModel.costPerXpToLevel = 0f;
+                    }
                 }
+
                 foreach (UpgradeModel upgrade in Game.instance.model.upgrades)
                 {
                     upgrade.cost = upgradeCost;
                 }
+            }
+        }
+
+        [HarmonyPatch(typeof(SimHero), "GetCostToLevelUp")]
+        public class HeroCost_Patch
+        {
+            [HarmonyPostfix]
+            public static void Postfix(ref float __result)
+            {
+                __result = (float)(double)heroUpgradeCost;
+            }
+        }
+
+        [HarmonyPatch(typeof(SimHero), "PurchaseHeroLevelUp")]
+        public class HeroPurchase_Patch
+        {
+            [HarmonyPrefix]
+            public static void Prefix()
+            {
+                buyingHeroLevel = true;
+            }
+
+            [HarmonyFinalizer]
+            public static void Finalizer()
+            {
+                buyingHeroLevel = false;
+            }
+        }
+
+        [HarmonyPatch(typeof(SimTowerManager), "UpgradeTower")]
+        public class UpgradeTower_Patch
+        {
+            [HarmonyPrefix]
+            public static void Prefix(ref float upgradeCost)
+            {
+                if (buyingHeroLevel) upgradeCost = (float)heroUpgradeCost;
             }
         }
     }
